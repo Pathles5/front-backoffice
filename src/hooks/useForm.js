0f18be8d81f5adf42ee4c522bf5dc8d86f39b9e9
alopez <alopez@soaint.com>
2023-12-21 10:14:42 +0100

    *

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -11,25 +11,16 @@ export default function useForm(/* {additionalData}*/) {
         setMessage('');
 
         const finalFormEndpoint = e.target.action;
-        console.log('Buscando el metodo');
-        console.log(e.target);
-        console.log(e.target.action);
-        console.log(e.target.method);
-        console.log(method);
         const data = Array.from(e.target.elements)
             .filter(element => element.id)
             .reduce((obj, element) => Object.assign(obj, { [element.id]: element.value }), {});
         
         const body = transformFormClient(data);
 
-        console.log('body');
-        console.log(body);
         // if (additionalData) {
         //     Object.assign(body,additionalData);
         // }
 
-        console.log('body');
-        console.log(body);
         // const client = Array.from(e.target.elements)
         //     .filter((input) => input.id)
         //     .reduce((obj, input) => Object.assign(obj, { [input.id]: input.value }), {});
@@ -42,7 +33,11 @@ export default function useForm(/* {additionalData}*/) {
             },
             body: JSON.stringify(body),
         }).then(response => {
+            setStatus('success')
+            setMessage('success')
             if (response.status !== 200) {
+                setStatus('error')
+                setMessage('success')
                 throw new Error(response.statusText);
             }
             return response.json();
